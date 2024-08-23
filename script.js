05ec0644facfa67bff44cfc177dab8921dd32349
jeunir23 <jeunir23@gmail.com>
2024-08-23 14:43:52 +0800

    loop all items in rows

--- script.js
+++ script.js
@@ -4,4 +4,8 @@ const rows = document.querySelectorAll("tbody tr");
 
 searchInput.addEventListener("keyup", function () {
   const filter = this.value.toLowerCase();
+
+  rows.forEach((row) => {
+    text = row.textContent.toLowerCase();
+  });
 });
