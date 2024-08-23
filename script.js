9a6e6fa9a996e769bd37c5bef072babdf8d46e24
jeunir23 <jeunir23@gmail.com>
2024-08-23 14:40:53 +0800

    added keup addeventlistiner and converted to lowercase

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 "use strict";
 const searchInput = document.querySelector(".search-input");
 const rows = document.querySelectorAll("tbody tr");
+
+searchInput.addEventListener("keyup", function () {
+  const filter = this.value.toLowerCase();
+});
