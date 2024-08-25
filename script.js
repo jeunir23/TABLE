e442603a000fb0fa9473ab011be9d50a0c21ae53
jeunir23 <jeunir23@gmail.com>
2024-08-25 21:16:02 +0800

    Final

--- script.js
+++ script.js
@@ -2,12 +2,10 @@
 const searchInput = document.querySelector(".search-input");
 const rows = document.querySelectorAll("tbody tr");
 
-searchInput.addEventListener("keyup", function () {
+searchInput.addEventListener("input", function () {
   const filter = this.value.toLowerCase();
-
   rows.forEach((row) => {
     const text = row.textContent.toLowerCase();
-
     if (text.indexOf(filter) > -1) {
       row.classList.remove("hide");
     } else {
